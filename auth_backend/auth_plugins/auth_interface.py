62d53f72aae4457f697aae3e7fa197725f0a75de
semen603089 <semen603089@mail.ru>
2022-10-05 08:51:44 +0300

    interface init

--- auth_backend/auth_plugins/auth_interface.py
+++ auth_backend/auth_plugins/auth_interface.py
@@ -1,5 +1,22 @@
 from abc import ABCMeta
 
+from auth_backend.models import Session
+
 
 class AuthInterface(metaclass=ABCMeta):
-    ...
+    """
+    Parameters:
+        auth_params which auth type need: like email, hashed_password and salt
+    """
+
+    def register(self) -> Session | None:
+        raise NotImplementedError()
+
+    def login(self) -> Session | None:
+        raise NotImplementedError()
+
+    def logout(self) -> None:
+        raise NotImplementedError()
+
+    def forgot_password(self) -> Session | None:
+        raise NotImplementedError()
---
+++ auth_backend/models/__init__.py
@@ -0,0 +1,3 @@
+from .db import User, Session, AuthMethod
+
+__all__ = ["User", "Session", "AuthMethod"]
