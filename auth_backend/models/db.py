a980cedce9e3db5e1dfb1019e01556376d095792
semen603089 <semen603089@mail.ru>
2022-10-05 07:52:22 +0300

    db

--- auth_backend/models/db.py
+++ auth_backend/models/db.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import datetime
 
 from .base import Base
@@ -7,6 +8,8 @@ import sqlalchemy.orm
 class User(Base):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 
+    auth_methods: list[AuthMethod] = sqlalchemy.orm.relationship("AuthMethod", foreign_keys=[id])
+    sessions: list[Session] = sqlalchemy.orm.relationship("Session", foreign_keys=[id])
 
 
 class AuthMethod(Base):
@@ -16,9 +19,13 @@ class AuthMethod(Base):
     param = sqlalchemy.Column(sqlalchemy.String)
     value = sqlalchemy.Column(sqlalchemy.String)
 
+    user: User = sqlalchemy.orm.relationship("User", foreign_keys=[user_id], back_populates="auth_methods")
+
 
 class Session(Base):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("User.id"))
     expires = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow() + datetime.timedelta(days=7))
     token = sqlalchemy.Column(sqlalchemy.String, unique=True)
+
+    user: User = sqlalchemy.orm.relationship("User", foreign_keys=[user_id], back_populates="sessions")
