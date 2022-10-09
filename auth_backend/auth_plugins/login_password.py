34e6c76cad33820893eb8c219ca7330ab0c930c9
semen603089 <semen603089@mail.ru>
2022-10-09 08:08:12 +0300

    login

--- auth_backend/auth_plugins/login_password.py
+++ auth_backend/auth_plugins/login_password.py
@@ -19,7 +19,6 @@ class LoginPassword(AuthInterface):
 
         @staticmethod
         def __hash_password(password: str, salt: str = None):
-            """ Хеширует пароль с солью """
             salt = salt or get_salt()
             enc = hashlib.pbkdf2_hmac("sha256", password.encode(), salt.encode(), 100_000)
             return enc.hex()
