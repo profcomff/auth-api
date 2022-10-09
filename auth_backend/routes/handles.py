8513624d36afab324ac7b4c326398fe5753d9b4d
semen603089 <semen603089@mail.ru>
2022-10-09 16:17:00 +0300

    base

--- auth_backend/routes/handles.py
+++ auth_backend/routes/handles.py
@@ -1,5 +1,24 @@
+from typing import Literal
+
 from fastapi import APIRouter
+from .models import Token, Email
 
 handles = APIRouter(prefix="", tags=["Auth"])
+
+
+@handles.post("/registration", response_model=Token)
+async def registration(type: Literal["2"], schema: Email) -> Token:
+    ...
+
+
+@handles.post("/login", response_model=Token)
+async def login(type: Literal["2"], schema: Email) -> Token:
+    ...
+
+
+@handles.post("/logout", response_model=None)
+async def logout(token: Token) -> None:
+    ...
+
 
 
