da39cd8c42cf85f86d01b4c8a507c9f1f9bd1c47
semen603089 <semen603089@mail.ru>
2022-10-09 08:22:48 +0300

    base

--- auth_backend/routes/base.py
+++ auth_backend/routes/base.py
@@ -1,3 +1,24 @@
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware
+from starlette.middleware.cors import CORSMiddleware
+
+from auth_backend.settings import get_settings
+
+settings = get_settings()
 
 app = FastAPI()
+
+
+app.add_middleware(
+    DBSessionMiddleware,
+    db_url=settings.DB_DSN,
+    session_args={"autocommit": True},
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOW_ORIGINS,
+    allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+    allow_methods=settings.CORS_ALLOW_METHODS,
+    allow_headers=settings.CORS_ALLOW_HEADERS,
+)
