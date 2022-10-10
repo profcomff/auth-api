e3b1a59c10f88fa862d8a17371390f455f7b697b
semen603089 <semen603089@mail.ru>
2022-10-10 18:50:18 +0300

    refact

--- auth_backend/routes/models.py
+++ auth_backend/routes/models.py
@@ -1,4 +1,14 @@
+import datetime
+
 from pydantic import BaseModel, EmailStr
+
+
+class Base(BaseModel):
+    def __repr__(self) -> str:
+        attrs = []
+        for k, v in self.__class__.schema().items():
+            attrs.append(f"{k}={v}")
+        return "{}({})".format(self.__class__.__name__, ', '.join(attrs))
 
 
 class Email(BaseModel):
@@ -8,3 +18,9 @@ class Email(BaseModel):
 
 class Token(BaseModel):
     token: str
+
+
+class Session(Token):
+    expires: datetime.datetime
+    id: int
+    user_id: int
