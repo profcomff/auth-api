c67cac23c65f3c5a74581f3fdfaf3406433c7282
semen603089 <semen603089@mail.ru>
2022-10-14 06:38:04 +0300

    эти комменты меня убивают

--- auth_backend/routes/login_password.py
+++ auth_backend/routes/login_password.py
@@ -1,11 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi_sqlalchemy import db
 
+from auth_backend.auth_plugins.login_password import LoginPassword
 from auth_backend.models.db import AuthMethod
-from auth_backend.models.db import Session as DbSession
-from auth_backend.auth_plugins.email_confirrmation import send_change_password_confirmation
-from .models.base import Session
-from pydantic import EmailStr
 from auth_backend.settings import get_settings
 
 settings = get_settings()
@@ -20,11 +17,12 @@ login_password = APIRouter(prefix="/email", tags=["Email"])
 
 @login_password.post("/approve/email", response_model=None)
 async def approve_email(token: str) -> None:
-    query: list[AuthMethod] = db.session.query(AuthMethod).filter(AuthMethod.token == token).all()
+    query: AuthMethod = db.session.query(AuthMethod).filter(AuthMethod.value == token).one_or_none()
     if not query:
-        raise Exception
-    for row in query:
-        row.is_active = True
+        raise HTTPException(status_code=403, detail="Incorrect link")
+    for row in query.user.get_auth_methods(LoginPassword.__name__):
+        if row.param == "confirmed":
+            row.value = True
     db.session.flush()
 
 
