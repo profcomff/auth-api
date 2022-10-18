24ed2546bf39ef7b82fdf4394823b64284aef0df
semen603089 <semen603089@mail.ru>
2022-10-18 21:16:22 +0300

    review

--- auth_backend/__init__.py
+++
@@ -1,4 +0,0 @@
-from .settings import get_settings
-from .exceptions import ObjectNotFound, IncorrectAuthType, AuthFailed, AlreadyExists
-
-__all__ = ["get_settings", "ObjectNotFound", "IncorrectAuthType", "AuthFailed", "AlreadyExists"]
