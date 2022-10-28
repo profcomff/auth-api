75100622d3266caeafee6f2c1256345e7be2badd
MikeP <parfenov.ma21@physics.msu.ru>
2022-10-28 23:04:00 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from fastapi.testclient import TestClient
 from auth_backend.routes.base import app
 from auth_backend.settings import get_settings
+from sqlalchemy_utils import create_database, database_exists, drop_database
 from uuid import uuid4
 from tests.utils import run_downgrade, run_upgrade
 
@@ -20,6 +21,8 @@ def postgres() -> str:
     tmp_name = f"{uuid4().hex}_pytest"
     settings.DB_DSN.replace(settings.DB_DSN.split('/')[-1], tmp_name)
     tmp_url = settings.DB_DSN
+    if not database_exists(tmp_url):
+        create_database(tmp_url)
     yield tmp_url
 
 
