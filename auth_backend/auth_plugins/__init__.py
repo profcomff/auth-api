dd0708b908d7cf6ea2f9a3bb1b7a4ca3f28322b9
Artem Morozov <morozoff234@yandex.ru>
2023-03-06 16:25:39 +0300

    Вход в приложение через аккаунт Yandex

--- auth_backend/auth_plugins/__init__.py
+++ auth_backend/auth_plugins/__init__.py
@@ -3,5 +3,5 @@ from .email import Email
 from .google import GoogleAuth
 from .physics import PhysicsAuth
 from .lkmsu import LkmsuAuth
-
+from .yandex import YandexAuth
 __all__ = ["AUTH_METHODS", "AuthMethodMeta", "Email"]
