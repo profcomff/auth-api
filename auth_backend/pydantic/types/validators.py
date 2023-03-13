4530786c1a3ef5ae81be2ecc937be1fb58f8a81e
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-03-13 20:29:11 +0300

    Update validators.py

--- auth_backend/pydantic/types/validators.py
+++ auth_backend/pydantic/types/validators.py
@@ -19,12 +19,12 @@ class Scope(str):
     @classmethod
     def validate(cls, val: str) -> str:
         if not val:
-            raise ValueError
-        val = str(val).strip()
+            raise ValueError("None or empty string are not allowed")
+        val = str(val).strip().lower()
         if val[0] == "." or val[-1] == ".":
-            raise ValueError
-        if len(set(val) - set(string.ascii_letters + ".")) > 0:
-            raise ValueError
+            raise ValueError("Dot can not be leading or closing")
+        if len(set(val) - set(string.ascii_lowercase + "._")) > 0:
+            raise ValueError("Only letters, dot and underscore allowed")
         return val
 
     __weakref__ = property(lambda self: object(), lambda self, v: None, lambda self: None)
