d3dc69c03d1fd26cee05f20992675bfe02bb4826
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-03-20 23:11:51 +0300

    Fix unique_google_id

--- auth_backend/auth_plugins/google.py
+++ auth_backend/auth_plugins/google.py
@@ -75,7 +75,7 @@ class GoogleAuth(OauthMeta):
             )
         except GoogleAuthError as exc:
             raise OauthCredentialsIncorrect(f'Google account response invalid: {exc}')
-        user = await cls._get_user('unique_google_id', guser_id, db_session=db.session)
+        user = await cls._get_user('unique_google_id', guser_id['sub'], db_session=db.session)
         if user is not None:
             raise AlreadyExists(User, user.id)
 
@@ -96,7 +96,7 @@ class GoogleAuth(OauthMeta):
             user = await cls._create_user(db_session=db.session) if user_session is None else user_session.user
         else:
             user = user_session.user
-        await cls._register_auth_method('unique_google_id', guser_id, user, db_session=db.session)
+        await cls._register_auth_method('unique_google_id', guser_id['sub'], user, db_session=db.session)
 
         return await cls._create_session(user, user_inp.scopes, db_session=db.session)
 
@@ -120,7 +120,7 @@ class GoogleAuth(OauthMeta):
             )
         except GoogleAuthError as exc:
             raise OauthCredentialsIncorrect(f'Google account response invalid: {exc}')
-        user = await cls._get_user('unique_google_id', guser_id, db_session=db.session)
+        user = await cls._get_user('unique_google_id', guser_id['sub'], db_session=db.session)
         if not user:
             raise OauthAuthFailed('No users found for google account', id_token=credentials.get("id_token"))
         return await cls._create_session(user, user_inp.scopes, db_session=db.session)
