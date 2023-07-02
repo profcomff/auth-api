6a09bb2a23fda1e549f11a9ac30c55c183faa763
semen603089 <semen603089@mail.ru>
2023-07-02 21:58:45 +0300

    fix

--- auth_backend/auth_plugins/github.py
+++ auth_backend/auth_plugins/github.py
@@ -8,7 +8,7 @@ from fastapi_sqlalchemy import db
 from pydantic import BaseModel, Field
 
 from auth_backend.exceptions import AlreadyExists, OauthAuthFailed
-from auth_backend.models.db import AuthMethod, UserSession
+from auth_backend.models.db import AuthMethod, UserSession, User
 from auth_backend.schemas.types.scopes import Scope
 from auth_backend.settings import Settings
 from auth_backend.utils.security import UnionAuth
@@ -100,7 +100,7 @@ class GithubAuth(OauthMeta):
         user = await cls._get_user('user_id', github_user_id, db_session=db.session)
 
         if user is not None:
-            raise AlreadyExists(user, user.id)
+            raise AlreadyExists(User, user.id)
         if user_session is None:
             user = await cls._create_user(db_session=db.session) if user_session is None else user_session.user
         else:
