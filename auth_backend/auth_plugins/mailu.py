c6c896eab7ef8fc32aaef7c203d0f01a4379970f
default <roman@dyakov.space>
2024-06-26 17:28:29 +0000

    Fix path

--- auth_backend/auth_plugins/mailu.py
+++ auth_backend/auth_plugins/mailu.py
@@ -16,7 +16,7 @@ class MailuOuterAuthSettings(Settings):
 
 
 class MailuOuterAuth(OuterAuthMeta):
-    prefix = '/airflow'
+    prefix = '/mailu'
     settings = MailuOuterAuthSettings()
 
     @classmethod
