9232dbec6b18d87f0fec3004438b41998201e614
default <roman@dyakov.space>
2024-06-26 17:06:29 +0000

    Fix session creation

--- auth_backend/cli/scope.py
+++ auth_backend/cli/scope.py
@@ -9,7 +9,6 @@ def create_scope(name: str, creator_id: int, comment: str, session: Session) -> 
     if Scope.query(session=session).filter(Scope.name == name).one_or_none():
         print("Scope already exists")
         exit(errno.EIO)
-    scope = Scope(name=name, creator_id=creator_id, comment=comment)
-    session.add(scope)
+    scope = Scope.create(name=name, creator_id=creator_id, comment=comment, session=session)
     session.commit()
     print(f"Created scope: {scope}")
