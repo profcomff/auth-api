f0e0baa28c3c83f06471e647f41a4019406b70da
Semyon Grigoriev <semen603089@mail.ru>
2024-07-23 16:34:30 +0300

    format

--- auth_backend/cli/scope.py
+++ auth_backend/cli/scope.py
@@ -9,7 +9,12 @@ def create_scope(name: str, creator_email: str, comment: str, session: Session) 
     if Scope.query(session=session).filter(Scope.name == name).one_or_none():
         print("Scope already exists")
         exit(errno.EIO)
-    creator_id = AuthMethod.query(session=session).filter(AuthMethod.auth_method == "email", AuthMethod.value == creator_email).one().user_id
+    creator_id = (
+        AuthMethod.query(session=session)
+        .filter(AuthMethod.auth_method == "email", AuthMethod.value == creator_email)
+        .one()
+        .user_id
+    )
     scope = Scope.create(name=name, creator_id=creator_id, comment=comment, session=session)
     session.commit()
     print(f"Created scope: {scope}")
--- auth_backend/cli/user_group.py
+++ auth_backend/cli/user_group.py
@@ -6,7 +6,12 @@ from auth_backend.models.db import AuthMethod, Group, UserGroup
 
 
 def create_user_group(email: str, session: Session) -> None:
-    user_id = AuthMethod.query(session=session).filter(AuthMethod.auth_method == "email", AuthMethod.value == email).one().user_id
+    user_id = (
+        AuthMethod.query(session=session)
+        .filter(AuthMethod.auth_method == "email", AuthMethod.value == email)
+        .one()
+        .user_id
+    )
     group_id = Group.query(session=session).filter(Group.name == "root").one().id
     if (
         UserGroup.query(session=session)
