1361a58f72ce7817e710ecdec1bdff580596f7a7
Grigoriev Semyon <33061489+grigoriev-semyon@users.noreply.github.com>
2024-07-23 16:36:38 +0300

    Cli fix (#194)

--- auth_backend/cli/process.py
+++ auth_backend/cli/process.py
@@ -40,14 +40,13 @@ def get_args():
     scope_subparsers = scope.add_subparsers(dest='subcommand')
     scope_create = scope_subparsers.add_parser("create")
     scope_create.add_argument('--name', type=str, required=True)
-    scope_create.add_argument('--creator', type=str, required=True)
+    scope_create.add_argument('--creator_email', type=str, required=True)
     scope_create.add_argument('--comment', type=str, required=True)
 
     user_group = subparsers.add_parser("user_group")
     user_group_subparsers = user_group.add_subparsers(dest='subcommand')
     user_group_create = user_group_subparsers.add_parser("create")
-    user_group_create.add_argument('--user_id', type=str, required=True)
-    user_group_create.add_argument('--group_id', type=str, required=True)
+    user_group_create.add_argument('--email', type=str, required=True)
 
     return parser.parse_args()
 
@@ -67,7 +66,7 @@ def process() -> None:
         create_group(args.name, args.scopes, args.parent, session)
     elif args.command == 'scope' and args.subcommand == 'create':
         print(f'Creating scope with params {args}')
-        create_scope(args.name, args.creator, args.comment, session)
+        create_scope(args.name, args.creator_email, args.comment, session)
     elif args.command == 'user_group' and args.subcommand == 'create':
         print(f'Creating user_group with params {args}')
-        create_user_group(args.user_id, args.group_id, session)
+        create_user_group(args.email, session)
