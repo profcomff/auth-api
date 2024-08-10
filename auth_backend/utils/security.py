bbf3a5e14eb1612f08951c6b47c112b34c561b81
MonTueVed <samolko.mon@gmail.com>
2024-08-10 22:01:34 +0300

    update expires

--- auth_backend/utils/security.py
+++ auth_backend/utils/security.py
@@ -21,7 +21,7 @@ class UnionAuth(SecurityBase):
     auto_error: bool
     allow_none: bool
     _scopes: list[str] = []
-    AUTH_SESSION_UPDATE_SCOPE = 'auth.session.update'
+    _SESSION_UPDATE_SCOPE = 'auth.session.update'
 
     def __init__(self, scopes: list[str] = None, allow_none=False, auto_error=False) -> None:
         super().__init__()
@@ -54,7 +54,7 @@ class UnionAuth(SecurityBase):
         if user_session.expired:
             self._except()
         session_scopes = set([scope.name.lower() for scope in user_session.scopes])
-        if self.AUTH_SESSION_UPDATE_SCOPE in session_scopes:
+        if self._SESSION_UPDATE_SCOPE in session_scopes:
             user_session.expires = session_expires_date()
         db.session.commit()
         if len(set([_scope.lower() for _scope in self._scopes]) & session_scopes) != len(set(self._scopes)):
