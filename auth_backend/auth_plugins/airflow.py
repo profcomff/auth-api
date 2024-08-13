110c07b5f61f7372bbf04386c6a345118f4abd95
default <z4haroff.ivan@yandex.com>
2024-08-13 10:57:02 +0000

    logginable deleting bug fix

--- auth_backend/auth_plugins/airflow.py
+++ auth_backend/auth_plugins/airflow.py
@@ -19,6 +19,7 @@ class AirflowOuterAuthSettings(Settings):
 class AirflowOuterAuth(OuterAuthMeta):
     prefix = '/airflow'
     settings = AirflowOuterAuthSettings()
+    loginable = False
 
     @classmethod
     async def _is_outer_user_exists(cls, username: str) -> bool:
