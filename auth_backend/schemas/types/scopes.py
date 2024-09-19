e6154d8038eeaaafbd456183ea33578a2f945fde
Timur Enikeev <163281083+DaymasS@users.noreply.github.com>
2024-09-19 13:52:05 -0400

    Pydantic 2.9 fix (#208)
    ## Изменения
    Фикс для Pydantic 2.9...
    
    ## Детали реализации
    Добавил обязательный хэндлер 
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?

--- auth_backend/schemas/types/scopes.py
+++ auth_backend/schemas/types/scopes.py
@@ -1,7 +1,7 @@
 import string
 from typing import Any
 
-from pydantic._internal import _schema_generation_shared
+from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler
 from pydantic.json_schema import JsonSchemaValue
 from pydantic_core import core_schema
 
@@ -19,12 +19,13 @@ class Scope:
     def __get_pydantic_core_schema__(
         cls,
         source: type[Any],
+        handler: GetCoreSchemaHandler,
     ) -> core_schema.CoreSchema:
         return core_schema.general_after_validator_function(cls._validate, core_schema.str_schema())
 
     @classmethod
     def __get_pydantic_json_schema__(
-        cls, core_schema: core_schema.CoreSchema, handler: _schema_generation_shared.GetJsonSchemaHandler
+        cls, core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
     ) -> JsonSchemaValue:
         field_schema = handler(core_schema)
         field_schema.update(type='string', format='scope')
