711c048b34ae43d3c82a7667887692e2e73bed3a
Timur Enikeev <enikeevt04@gmail.com>
2024-11-25 10:08:58 -0500

    Default unbounded to false

--- auth_backend/utils/user_session_control.py
+++ auth_backend/utils/user_session_control.py
@@ -20,7 +20,7 @@ async def create_session(
     scopes_list_names: list[TypeScope] | None,
     expires: datetime | None = None,
     session_name: str | None = None,
-    is_unbounded: bool | None = None,
+    is_unbounded: bool = False,
     *,
     db_session: DbSession,
 ) -> Session:
@@ -34,7 +34,7 @@ async def create_session(
         user_id=user.id, token=random_string(length=settings.TOKEN_LENGTH), session_name=session_name
     )
     user_session.expires = expires or user_session.expires
-    user_session.is_unbounded = user_session.is_unbounded if is_unbounded is None else is_unbounded
+    user_session.is_unbounded = is_unbounded
     db_session.add(user_session)
     db_session.flush()
     if not user_session.is_unbounded:
