1b4226bdf9f9cce7bcf1c0bc5f5d18d2bd485a89
Roman Dyakov <roman@dyakov.space>
2025-03-13 16:22:29 +0000

    Token by password

--- auth_backend/routes/oidc.py
+++ auth_backend/routes/oidc.py
@@ -2,15 +2,14 @@ import logging
 from datetime import datetime
 from typing import Annotated, Literal, Optional
 
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, BackgroundTasks, Form, Header
 from fastapi_sqlalchemy import db
 
-from auth_backend.exceptions import SessionExpired
 from auth_backend.models.db import Scope, UserSession
 from auth_backend.schemas.oidc import PostTokenResponse
 from auth_backend.settings import get_settings
 from auth_backend.utils.jwt import create_jwks
-from auth_backend.utils.user_session_control import SESSION_UPDATE_SCOPE, create_session
+from auth_backend.utils.oidc_token import token_by_refresh_token, token_by_client_credentials
 
 
 settings = get_settings()
@@ -34,7 +33,10 @@ def openid_configuration():
         "subject_types_supported": ["public"],
         "id_token_signing_alg_values_supported": ["RS256"],
         "claims_supported": ["sub", "iss", "exp", "iat"],
-        # "authorization_endpoint": f"{settings.APPLICATION_HOST}/auth",
+        "grant_types_supported": [
+            "refresh_token",
+            "client_credentials",
+        ],
     }
 
 
@@ -46,56 +48,58 @@ def jwks():
 
 @router.post("/token")
 async def token(
-    grant_type: Annotated[Literal['refresh_token'], Form()],
-    client_id: Annotated[Literal['app'], Form()],  # Тут должна быть любая строка, которую проверяем в БД
+    background_tasks: BackgroundTasks,
+    # Общие OIDC параметры
+    user_agent: Annotated[str, Header()],
+    grant_type: Annotated[Literal['refresh_token', 'client_credentials'], Form()],
+    client_id: Annotated[str, Form()],  # Тут должна быть любая строка, которую проверяем в БД
     client_secret: Annotated[Optional[str], Form()] = None,
+    scopes: Annotated[list[str] | None, Form()] = None,
+    # grant_type=refresh_token
     refresh_token: Annotated[Optional[str], Form()] = None,
+    # grant_type=client_credentials
+    username: Annotated[Optional[str], Form()] = None,
+    password: Annotated[Optional[str], Form()] = None,
 ) -> PostTokenResponse:
     """Ручка для получения токена доступа
 
-    Позволяет:
+    ## Позволяет
     - Обменять старый не-JWT токен на новый c таким же набором доступов и таким же сроком давности
     - Обменять JWT токен на новый, если у него есть SESSION_UPDATE_SCOPE
 
     Потенциально будет позволять:
     - Обменивать Refresh Token на пару Access Token + Refresh Token
     - Обменивать Code (см. Oauth Authorization Code Flow) на пару Access Token + Refresh Token
+
+    ## Параметры:
+    Для всех запросов
+    - `grant_type` – refresh_token/client_credentials (см. список в `/.well_known/openid_configuration` в поле `grant_types_supported`)
+    - `client_id` – строка, по которой проверяется принадлежность к проекту (сейчас только app)
+    - `scopes` – список прав для нового токена
+
+    ### `grant_type=refresh_token`
+    - refresh_token – токен, выданный этой ручкой или ручкой `/login` в методе авторизации
+
+    ### `grant_type=client_credentials`
+    - `username` – логин пользователя
+    - `password` – пароль пользователя
     """
+    scopes = scopes or []
+
+    if client_id != 'app':
+        raise NotImplementedError("Only app client id supported")
     if grant_type == 'authorization_code':
         raise NotImplementedError("Authorization Code Flow not implemented yet")
+
+    # Разные методы обмена токенов
     if grant_type == "refresh_token":
-        # Все токены автоматически считаем refresh-токенами
-        if not refresh_token:
-            raise TypeError("refresh_token required for refresh_token grant_type ")
-        old_session: UserSession = (
-            UserSession.query(session=db.session).filter(UserSession.token == refresh_token).one_or_none()
-        )
-        if not old_session or old_session.expired:
-            raise SessionExpired()
+        new_session = await token_by_refresh_token(refresh_token, scopes)
+    if grant_type == "refresh_token":
+        new_session = await token_by_client_credentials(username, password, scopes, user_agent, background_tasks)
 
-        # Продлеваем только те токены, которые явно разрешено продлевать
-        # Остальные просто заменяем на новые с тем же сроком действия
-        session_scopes = old_session.user.scope_names if old_session.is_unbounded else old_session.scope_names
-        expire_ts = None
-        if SESSION_UPDATE_SCOPE not in session_scopes:
-            expire_ts = old_session.expires
-
-        new_session = await create_session(
-            old_session.user,
-            session_scopes,
-            expire_ts,
-            old_session.session_name,
-            old_session.is_unbounded,
-            db_session=db.session,
-        )
-
-        # Старую сессию убиваем
-        old_session.expires = datetime.utcnow()
-        db.session.commit()
-
-        return PostTokenResponse(
-            access_token=new_session.token,
-            token_type="Bearer",
-            expires_in=int((new_session.expires - datetime.utcnow()).total_seconds()),
-            refresh_token=new_session.token,
-        )
+    return PostTokenResponse(
+        access_token=new_session.token,
+        token_type="Bearer",
+        expires_in=int((new_session.expires - datetime.utcnow()).total_seconds()),
+        refresh_token=new_session.token,
+    )
---
+++ auth_backend/utils/oidc_token.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from fastapi import BackgroundTasks
+from fastapi_sqlalchemy import db
+from auth_backend.models.db import UserSession, Scope
+from auth_backend.exceptions import SessionExpired, AuthFailed
+from auth_backend.utils.user_session_control import SESSION_UPDATE_SCOPE, create_session
+from auth_backend.schemas.models import Session as SessionSchema
+
+
+async def token_by_refresh_token(
+    refresh_token: str | None,
+    requested_scopes: list[str] | None,
+    _: BackgroundTasks,
+) -> SessionSchema:
+    # Все токены автоматически считаем refresh-токенами
+    if not refresh_token:
+        raise TypeError("refresh_token required for refresh_token grant_type ")
+    old_session: UserSession = (
+        UserSession.query(session=db.session).filter(UserSession.token == refresh_token).one_or_none()
+    )
+    if not old_session or old_session.expired:
+        raise SessionExpired()
+
+    # Продлеваем только те токены, которые явно разрешено продлевать
+    # Остальные просто заменяем на новые с тем же сроком действия
+    session_scopes = old_session.user.scope_names if old_session.is_unbounded else old_session.scope_names
+
+    # Если запрошены скоупы, то выдать новый токен с запрошенными скоупами, если у текущего хватает прав
+    if requested_scopes:
+        requested_scopes = set(requested_scopes)
+        if requested_scopes > session_scopes:
+            raise AuthFailed("Don't have enough permissions to get scopes: ", requested_scopes - session_scopes)
+        session_scopes = requested_scopes - session_scopes
+
+    # Продлить действие токена, если сессия это позволяет
+    expire_ts = None
+    if SESSION_UPDATE_SCOPE not in session_scopes:
+        expire_ts = old_session.expires
+
+    new_session = await create_session(
+        old_session.user,
+        session_scopes,
+        expire_ts,
+        old_session.session_name,
+        old_session.is_unbounded,
+        db_session=db.session,
+    )
+
+    # Старую сессию убиваем
+    old_session.expires = datetime.utcnow()
+    db.session.commit()
+
+    return new_session
+
+
+async def token_by_client_credentials(
+    username: str | None,
+    password: str | None,
+    scopes: list[str] | None,
+    user_agent: str,
+    background_tasks: BackgroundTasks,
+) -> SessionSchema:
+    from auth_backend.auth_plugins.email import Email
+
+    if not username or not password:
+        raise Exception
+
+    return await Email.login(
+        username,
+        password,
+        Scope.get_by_names(scopes),
+        session_name=user_agent,
+        background_tasks=background_tasks,
+    )
