6570f0261147fd10d5be2528860a1e6557c8d198
petrCher <88943157+petrCher@users.noreply.github.com>
2026-05-10 17:51:25 +0300

    soft deletes added

--- auth_backend/admin/admin.py
+++ auth_backend/admin/admin.py
@@ -1,4 +1,7 @@
 from sqladmin import ModelView
+from sqlalchemy import func, select
+from sqlalchemy.sql.expression import Select
+from starlette.requests import Request
 
 from auth_backend.models.db import Group, Scope, User
 from auth_backend.routes.user import patch_user_groups
@@ -7,7 +10,7 @@ from auth_backend.routes.user import patch_user_groups
 class ScopeAdmin(ModelView, model=Scope):
     name = "Scope"
     name_plural = "Scopes"
-    column_list = ["id", "name", "comment", "is_deleted"]
+    column_list = ["id", "name", "comment"]
     column_details_list = [
         "id",
         "name",
@@ -18,16 +21,34 @@ class ScopeAdmin(ModelView, model=Scope):
         "is_deleted",
     ]
     column_searchable_list = ["id", "name"]
-    column_sortable_list = ["id", "name", "is_deleted"]
+    column_sortable_list = ["id", "name"]
     column_default_sort = [("id", False)]
     form_excluded_columns = ["create_ts", "update_ts", "groups", "user_sessions", "is_deleted"]
     can_create = False  # I don't know how to use UnionAuth there to get user_id that is required
+
+    def list_query(self, request: Request) -> Select:
+        return select(Scope).where(Scope.is_deleted == False)
+
+    def count_query(self, request: Request) -> Select:
+        return select(func.count(Scope.id)).where(Scope.is_deleted == False)
+
+    async def update_model(self, request, pk, data):
+        with self.session_maker(expire_on_commit=False) as session:
+            scope_data = {k: v for k, v in data.items() if v is not None}
+            obj = Scope.update(int(pk), **scope_data, session=session)
+            session.commit()
+            return obj
+
+    async def delete_model(self, request, pk):
+        with self.session_maker(expire_on_commit=False) as session:
+            Scope.delete(session=session, id=int(pk))
+            session.commit()
 
 
 class GroupAdmin(ModelView, model=Group):
     name = "Group"
     name_plural = "Groups"
-    column_list = ["id", "name", "scopes", "users", "parent_id", "is_deleted"]
+    column_list = ["id", "name", "scopes", "users", "parent_id"]
     column_details_list = [
         "id",
         "name",
@@ -42,6 +63,12 @@ class GroupAdmin(ModelView, model=Group):
     column_sortable_list = ["id", "name", "parent_id", "is_deleted"]
     column_default_sort = [("id", False)]
     form_excluded_columns = ["create_ts", "update_ts", "is_deleted"]
+
+    def list_query(self, request: Request) -> Select:
+        return select(Group).where(Group.is_deleted == False)
+
+    def count_query(self, request: Request) -> Select:
+        return select(func.count(Group.id)).where(Group.is_deleted == False)
 
 
 class UserAdmin(ModelView, model=User):
@@ -62,7 +89,14 @@ class UserAdmin(ModelView, model=User):
         "scopes": lambda m, a: ", ".join(s.name for s in (m.scopes or set())),
     }
 
-    async def on_model_change(self, data: dict, model: User, is_created: bool, request) -> None:
+    def list_query(self, request: Request) -> Select:
+        return select(User).where(User.is_deleted == False)
+
+    def count_query(self, request: Request) -> Select:
+        return select(func.count(User.id)).where(User.is_deleted == False)
+
+    async def update_model(self, request, pk, data):
         group_ids = [int(group) for group in (data.pop("groups") or [])]
         with self.session_maker(expire_on_commit=False) as session:
-            patch_user_groups(model.id, group_ids, session)
+            patch_user_groups(int(pk), group_ids, session)
+            return User.get(int(pk), session=session)
