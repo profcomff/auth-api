4eb8b69bd7ebb9d7b7e13d3a195bb12ee3786533
Hollopenew <108941057+Hollopenew@users.noreply.github.com>
2026-08-09 18:44:26 +0300

    fix: add proper email validation regex

--- auth_backend/auth_plugins/email.py
+++ auth_backend/auth_plugins/email.py
@@ -1,5 +1,6 @@
 import hashlib
 import logging
+import re
 from typing import Annotated, Self
 
 from annotated_types import MinLen
@@ -26,36 +27,11 @@ settings = get_settings()
 logger = logging.getLogger(__name__)
 
 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9_.+\-%]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$')
+
+
 def check_email(v):
-    restricted: set[str] = {
-        '"',
-        '#',
-        '&',
-        "'",
-        '(',
-        ')',
-        '*',
-        ',',
-        '/',
-        ';',
-        '<',
-        '>',
-        '?',
-        '[',
-        '\\',
-        ']',
-        '^',
-        '`',
-        '{',
-        '|',
-        '}',
-        '~',
-        '\n',
-        '\r',
-    }
-    if "@" not in v:
-        raise ValueError()
-    if set(v) & restricted:
+    if not EMAIL_REGEX.match(v):
         raise ValueError()
     return v
 
